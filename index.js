d5d9bf966df4b27c1a76d3d550dfb6d81ef8d510
Mah-eshh <92861@fhss.sjp.ac.lk>
2021-12-12 00:06:46 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+app.use("/user", userRouter);
+
+app.get('/',(req,res) => {
+  res.send('It is Running !....');
+});
+
+const CONNECTION_URL =
+  "mongodb+srv://mernstack:mernstack@mern-webapp.xarr4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server runing on port: ${PORT}`))
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
